d8b4c3ac37b6ed96e26565fdee374e3aaa42adc9
charlielovett <115426211+charlielovett@users.noreply.github.com>
2024-09-30 12:05:55 -0500

    fetch data

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,13 +1,13 @@
 const CourseList = ({ courses }) => (
-    <div className="course-list">
+    <div className="course-list gap-2">
         {Object.entries(courses).map(([courseId, course]) => (
             <div key={courseId}>
-                <div className="card h-100 m-2">
+                <div className="card h-100">
                     <div className="card-body d-flex flex-column">
                         <h5 className="card-title">{course.term} CS {course.number}</h5>
                         <p className="card-text flex-grow-1">{course.title}</p>
                         <hr className="mt-auto"></hr>
-                        <p className="card-text">{course.meets}</p>
+                        <p className="card-text mt-auto">{course.meets}</p>
                     </div>
                 </div>
             </div>
